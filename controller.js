82a1b75b6d65e839fe4f02a16cbb0a8299ca564f
yogi <dimas.yogi.777@gmail.com>
2021-07-04 06:56:52 +0700

    buat rputes

--- controller.js
+++ controller.js
@@ -1,2 +1,7 @@
 'use strict';
-var response = require('/res')
+var response = require('./res');
+var connection = require('./koneksi');
+
+exports.index =  function(req,res){
+    response.ok('Aplikasi Rest API aku berjalan !')
+}
