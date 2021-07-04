a791e110f35019388930611d21dfebb40cce01fa
yogi <dimas.yogi.777@gmail.com>
2021-07-04 07:13:17 +0700

    berhasil menampilkasn data mahasiswa

--- controller.js
+++ controller.js
@@ -3,5 +3,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index =  function(req,res){
-    response.ok('Aplikasi Rest API aku berjalan !')
+    response.ok('Aplikasi Rest API aku berjalan !',res)
+}
+
+//menampilkan semua data mahasiswa
+exports.tampilSemuaMahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error,rows,fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    })
 }
