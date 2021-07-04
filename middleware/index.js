971a59a61f7c03845b8cacdefcc4de3eab83860d
yogi <dimas.yogi.777@gmail.com>
2021-07-04 15:11:08 +0700

    role

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
-var auth = require('./auth')
+var auth = require('./auth');
+const verifikasi = require('./verfikasi');
 var router = express.Router();
 
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
+router.post('/api/v1/login', auth.login);
+router.get('/api/v1/rahasia',verifikasi(),auth.halamanrahasia);
+//alamat yang perlu otorisasi;
 
 module.exports=router;
